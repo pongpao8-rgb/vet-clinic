fbbc812121f0b2d372ed4df1ea3719b0c0fc3ca9
pongpao8-rgb <pongpao8@gmail.com>
2026-05-25 18:22:59 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+javascriptexport const metadata = {
+  title: 'CCAH Vet Clinic',
+  description: 'Veterinary Management System',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
